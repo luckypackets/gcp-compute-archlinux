4cb3f85c5bf29298286f424063e850c8a0ec5889
shaan. <46220449+luckypackets@users.noreply.github.com>
2025-06-16 08:49:22 +0200

    Delete updatearch.sh

--- updatearch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mv /etc/pacman.conf /etc/pacman.conf.old
-sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
-sudo curl -o /etc/pacman.conf https://raw.githubusercontent.com/luckypackets/gcp-compute-archlinux/refs/heads/main/pacman.conf
-sudo curl -o /etc/pacman.d/mirrorlist https://raw.githubusercontent.com/luckypackets/gcp-compute-archlinux/refs/heads/main/mirrorlist
-sudo pacman -Syy
-sudo pacman -S archlinux-keyring
-sudo pacman-key --refresh-keys
