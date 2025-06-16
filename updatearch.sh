61db4382c240b7e9b660817aabdf36a70ff9128f
shaan. <46220449+luckypackets@users.noreply.github.com>
2025-06-16 08:33:23 +0200

    Update updatearch.sh

--- updatearch.sh
+++ updatearch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mv /etc/pacman.conf /etc/pacman.conf.old
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
-curl -o /etc/pacman.conf https://github.com/luckypackets/gcp-compute-archlinux/blob/81757ec3a26900347e080279e69821fdbf196169/pacman.conf
-curl -o /etc/pacman.d/mirrorlist https://github.com/luckypackets/gcp-compute-archlinux/blob/81757ec3a26900347e080279e69821fdbf196169/mirrorlist
-pacman -Syy
-pacman -S archlinux-keyring
-pacman-key --refresh-keys
+sudo mv /etc/pacman.conf /etc/pacman.conf.old
+sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+sudo curl -o /etc/pacman.conf https://raw.githubusercontent.com/luckypackets/gcp-compute-archlinux/refs/heads/main/pacman.conf
+sudo curl -o /etc/pacman.d/mirrorlist https://raw.githubusercontent.com/luckypackets/gcp-compute-archlinux/refs/heads/main/mirrorlist
+sudo pacman -Syy
+sudo pacman -S archlinux-keyring
+sudo pacman-key --refresh-keys
